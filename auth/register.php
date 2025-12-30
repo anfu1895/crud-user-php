b955b0d4782891d7f2f121fd8946497194910559
anfu1895 <angel95fuentes18@gmail.com>
2025-12-30 17:10:08 -0600

    refactor(register): remove unused success variable and add header location

--- auth/register.php
+++ auth/register.php
@@ -2,7 +2,6 @@
   require_once __DIR__ . '/../config/db.php';
 
   $errors = [];
-  $success = false;
 
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
@@ -32,7 +31,8 @@
           ':password' => $password
         ]);
 
-        $success = true;
+        header('Location: login.php', true, 302);
+        exit();
       }
     }
   }
